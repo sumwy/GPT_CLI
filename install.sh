7bb7f2cde3b15422c8fd30b998a778bda24f098d
sonwooyoung <13060373+sumwy@users.noreply.github.com>
2023-12-18 18:03:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,49 +1,38 @@
 #!/bin/bash
 
-
-# 사용자에게 OpenAI API 키 입력 요청
-read -p "Enter your OpenAI API key: " OPENAI_API_KEY
-
-# API 키를 파일에 저장
-echo $OPENAI_API_KEY > openai_api_key.txt
-
-# 저장소 URL 및 이름 설정
 REPO_URL="https://github.com/sumwy/GPT_CLI.git"
-REPO_NAME="gptcli"
+REPO_NAME="GPT_CLI"
 INSTALL_DIR="/usr/local/bin"
 
-# 저장소가 이미 존재하는지 확인
+# 저장소 클론 또는 기존 디렉토리 사용
 if [ -d "$REPO_NAME" ]; then
-    echo "Directory $REPO_NAME already exists."
-    # 기존 디렉토리를 사용하려면 주석 처리를 해제하세요.
+    echo "Directory $REPO_NAME already exists. Using the existing directory."
     cd $REPO_NAME
-    # 기존 디렉토리를 삭제하고 새로 클론하려면 다음 두 줄의 주석을 해제하세요.
-    rm -rf $REPO_NAME
-    git clone $REPO_URL
 else
-    # 저장소 클론
     echo "Cloning the repository..."
     git clone $REPO_URL
     cd $REPO_NAME
 fi
 
-# requirements.txt 파일 존재 여부 확인
+# 의존성 설치
 if [ -f "requirements.txt" ]; then
-    # 필요한 의존성 설치
     echo "Installing dependencies..."
     pip install -r requirements.txt
 else
-    echo "ERROR: requirements.txt not found. Aborting installation."
+    echo "ERROR: requirements.txt not found."
     exit 1
 fi
 
-# 실행 파일을 적절한 위치에 복사
+# Python 스크립트 복사 및 실행 권한 부여
 echo "Copying the script to the installation directory..."
 cp GPT_CLI.py $INSTALL_DIR/n2lcmd
-
-# 실행 권한 부여
 chmod +x $INSTALL_DIR/n2lcmd
 
-# 설치 완료 메시지
-echo "Installation completed successfully. You can run the script using 'n2lcmd' command."
+# OpenAI API 키 입력 요청 및 파일에 저장
+read -p "Enter your OpenAI API key: " OPENAI_API_KEY
+echo $OPENAI_API_KEY > openai_api_key.txt
 
+# Python 스크립트 실행
+python3 $INSTALL_DIR/n2lcmd
+
+echo "Installation and execution completed successfully."
