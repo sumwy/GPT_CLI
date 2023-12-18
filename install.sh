c1169ca3df5412e85d943747ff8a146c33c09637
sonwooyoung <13060373+sumwy@users.noreply.github.com>
2023-12-18 17:01:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,23 @@ export OPENAI_API_KEY
 
 # 저장소 URL 및 이름 설정
 REPO_URL="https://github.com/sumwy/GPT_CLI.git"
-REPO_NAME="gpt_cli"
+REPO_NAME="gpt_cli"
 INSTALL_DIR="/usr/local/bin"
 
 # 저장소가 이미 존재하는지 확인
 if [ -d "$REPO_NAME" ]; then
-    echo "Directory $REPO_NAME already exists. Using the existing directory."
+    echo "Directory $REPO_NAME already exists."
+    # 기존 디렉토리를 사용하려면 주석 처리를 해제하세요.
+    cd $REPO_NAME
+    # 기존 디렉토리를 삭제하고 새로 클론하려면 다음 두 줄의 주석을 해제하세요.
+    rm -rf $REPO_NAME
+    git clone $REPO_URL
 else
     # 저장소 클론
     echo "Cloning the repository..."
     git clone $REPO_URL
+    cd $REPO_NAME
 fi
-
-# 클론된 디렉토리로 이동
-cd $REPO_NAME
 
 # requirements.txt 파일 존재 여부 확인
 if [ -f "requirements.txt" ]; then
@@ -40,3 +43,4 @@ chmod +x $INSTALL_DIR/n2lcmd
 
 # 설치 완료 메시지
 echo "Installation completed successfully. You can run the script using 'n2lcmd' command."
+
